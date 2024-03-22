49a786cc0263248a67064895a21aea6ce9346b9a
Amanda Bruncrona <amanda_bruncrona@hotmail.com>
2024-03-22 19:43:34 +0200

    Bash files

--- test.bash
+++ test.bash
@@ -1,17 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=SURROGATE_WORKFLOW
-#SBATCH --account=project_2009007
-#SBATCH --time=00:10:00
+#SBATCH --account=project_2005083
+#SBATCH --time=01:00:00
 #SBATCH --partition=interactive
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --out=./test.out
-#SBATCH --err=./test.err
 
 module purge 
 module load python-data 
 
 # source $WRKDIR/.venv/bin/activate
-# export PYTHONUNBUFFERED=1
+
 export PYTHONPATH=$PYTHONPATH:$WRKDIR/src
-srun python3 -u tests/simple/run.py
+srun python3 src/run.py
