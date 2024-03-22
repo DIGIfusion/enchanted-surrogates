29a5672aaf1aae1331abf649e98727faee4dc0cc
Adam Kit <adam.kit@helsinki.fi>
2024-03-22 14:35:59 +0200

    TGLF runs weee

--- src/parsers/TGLFparser.py
+++ src/parsers/TGLFparser.py
@@ -1,4 +1,6 @@
 from .base import Parser
+import subprocess 
+import os 
 
 class TGLFparser(Parser):
     """ An I/O parser for TGLF """ 
@@ -6,6 +8,11 @@ class TGLFparser(Parser):
         pass 
 
     def write_input_file(self, params: dict, run_dir: str):
-        print(run_dir)
+        # give some parameters write to a new input file! 
+        if os.path.exists(run_dir): 
+            input_fpath = os.path.join(run_dir, 'input.tglf')
+            subprocess.run(['touch', f'{input_fpath}'])
+        else: 
+            raise FileNotFoundError(f'Couldnt find {run_dir}')
         print('Writing to', run_dir)
         
--- src/runners/TGLFrunner.py
+++ src/runners/TGLFrunner.py
@@ -3,7 +3,7 @@
 import numpy as np
 from .base import Runner
 import parsers.TGLFparser as tglfparser
-
+import subprocess
 class TGLFrunner(Runner):
     def __init__(self, *args, **kwargs): 
         self.parser = tglfparser()
@@ -11,7 +11,22 @@ class TGLFrunner(Runner):
 
     def single_code_run(self, params: dict, run_dir: str):
         """ Logic to run TGLF """
+        """ 
+        # TODO: change run_dir? 
+        run_dir, as it is now, is the full path, 
+        so we need to extract just the db_dir + simdir TGLF
+        """
+        # write input file
         self.parser.write_input_file(params, run_dir)
+        
+        # process input file         
+        tglf_sim_dir = '/'.join(run_dir.split('/')[-2:])
+        subprocess.run(['tglf', '-i', f'{tglf_sim_dir}'])
+
+        # run code
+        subprocess.run(['tglf', '-e', f'{tglf_sim_dir}'])
+
+        # process code
         return True 
 
     
