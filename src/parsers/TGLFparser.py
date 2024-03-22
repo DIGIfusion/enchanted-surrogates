58635cbdf7683ba4acdf66175a8d790d38df2685
Adam Kit <adam.kit@helsinki.fi>
2024-03-22 22:47:55 +0200

    sneaking in a TGLF commit on this branch

--- src/parsers/TGLFparser.py
+++ src/parsers/TGLFparser.py
@@ -11,14 +11,21 @@ class TGLFparser(Parser):
         self.growth_rate_freq_file = 'out.tglf.eigenvalue_spectrum'
         self.flux_spectrum_file = 'out.tglf.sum_flux_spectrum'
 
+    
     def write_input_file(self, params: dict, run_dir: str):
         # give some parameters write to a new input file! 
+        # TODO: write a standard input file based on somthing? 
         print('Writing to', run_dir)
         if os.path.exists(run_dir): 
             input_fpath = os.path.join(run_dir, 'input.tglf')
             subprocess.run(['touch', f'{input_fpath}'])
         else: 
             raise FileNotFoundError(f'Couldnt find {run_dir}')
+
+        with open(input_fpath, 'w') as file: 
+            for param_name, val in params.items(): 
+                file.write(f'{param_name}={val}\n')
+        # TODO: check for input comparisons with available inputs for code! 
 
     def read_output_file(self, run_dir: str):         
         ky_spectrum_file_path = os.path.join(run_dir, self.ky_spectrum_file)
