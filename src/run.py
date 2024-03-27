527cf41aef5d7fce6710541b6cda29b456423736
Amanda <amanda_bruncrona@hotmail.com>
2024-03-27 16:17:36 +0200

    run.py linting fixes

--- src/run.py
+++ src/run.py
@@ -1,9 +1,10 @@
 # run.py
-import yaml 
-
+import yaml
 import samplers
-import runners 
-import executors 
+# import runners
+import executors
+import argparse
+
 
 def load_configuration(config_path):
     with open(config_path, 'r') as file:
@@ -13,20 +14,21 @@ def load_configuration(config_path):
 
 
 def main(args):
-    sampler = getattr(samplers, args.sampler.pop('type'))(**args.sampler) 
-    # runner = getattr(runners, args.runner.pop('type'))(**args.runner) 
-    executor = getattr(executors, args.executor.pop('type'))(sampler, args.runner, **args.executor)  
+    sampler = getattr(samplers, args.sampler.pop('type'))(**args.sampler)
+    # runner = getattr(runners, args.runner.pop('type'))(**args.runner)
+    executor = getattr(executors, args.executor.pop('type'))(
+        sampler, args.runner, **args.executor)
 
     # executor.initialize(sampler=sampler, runner=runner, parser=parser)
     executor.start_runs()
 
-import argparse 
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runner')
-    parser.add_argument('-cf', '--config_file', type=str, default='base', help='name of configuration file!')
-    config_args = parser.parse_args() 
+    parser.add_argument(
+        '-cf', '--config_file', type=str, default='base',
+        help='name of configuration file!')
+    config_args = parser.parse_args()
     # config_path = "/scratch/project_2007159/cursed-tglf/src/test.yaml"
     args = load_configuration(config_args.config_file)
     main(args)
--- tests/simple/run.py
+++ tests/simple/run.py
@@ -1,11 +1,11 @@
 # run.py
-import yaml 
-import sys
+import yaml
+import argparse
+# import sys
+from ...src import samplers
+# from ...src import runners
+from ...src import executors
 
-sys.path.append('/scratch/project_2009007/enchanted-surrogates/src')
-import samplers
-import runners 
-import executors 
 
 def load_configuration(config_path):
     with open(config_path, 'r') as file:
@@ -14,16 +14,17 @@ def load_configuration(config_path):
 
 
 def main(args):
-    sampler = getattr(samplers, args.sampler.pop('type'))(**args.sampler) 
-    # runner = getattr(runners, args.runner.pop('type'))(**args.runner) 
-    executor = getattr(executors, args.executor.pop('type'))(sampler, args.runner, **args.executor)  
+    sampler = getattr(samplers, args.sampler.pop('type'))(**args.sampler)
+    # runner = getattr(runners, args.runner.pop('type'))(**args.runner)
+    executor = getattr(executors, args.executor.pop('type'))(
+        sampler, args.runner, **args.executor)
     # executor.initialize(sampler=sampler, runner=runner, parser=parser)
     executor.start_runs()
 
-import argparse 
+
 if __name__ == "__main__":
     # TODO: this should be argument with argparse
-    config_path = "/scratch/project_2009007/enchanted-surrogates/tests/simple/test.yaml"
+    config_path = "/enchanted-surrogates/tests/simple/test.yaml"
     config = load_configuration(config_path)
     args = argparse.Namespace(**config)
     main(args)
