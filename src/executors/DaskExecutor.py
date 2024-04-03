174fd66088c6443715a43dee74479c3744d991b4
Adam Kit <adam.kit@helsinki.fi>
2024-04-03 12:05:57 +0300

    whitespace bs

--- src/executors/DaskExecutor.py
+++ src/executors/DaskExecutor.py
@@ -8,8 +8,8 @@ from .base import Executor
 class DaskExecutor(Executor):
     def __init__(self, num_workers, worker_args, **kwargs):
         super().__init__(**kwargs)
-        self.num_workers = num_workers # kwargs.get('num_workers')
-        self.worker_args = worker_args # kwargs.get('worker_args')
+        self.num_workers = num_workers  # kwargs.get('num_workers')
+        self.worker_args = worker_args  # kwargs.get('worker_args')
         print('Beginning SLURMCluster Generation')
 
         # Create the SLURMCluster and define the resources for each of the
