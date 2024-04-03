f0280ef81b0d65fa2ff9e68e5c1360af663eb1ad
Adam Kit <adam.kit@helsinki.fi>
2024-04-03 11:31:25 +0300

    created a base class for the executors

--- src/executors/DaskExecutor.py
+++ src/executors/DaskExecutor.py
@@ -1,23 +1,12 @@
 # executors/DaskExecutor.py
 
 import os
-from dask.distributed import Client, as_completed
+from dask.distributed import Client
 from dask_jobqueue import SLURMCluster
-import uuid
-import runners
-# from dask import delayed, compute
+from .base import Executor
 
 
-def run_simulation_task(runner_args, params_from_sampler, base_run_dir):
-    print("Making Run dir")
-    run_dir = os.path.join(base_run_dir, str(uuid.uuid4()))
-    os.mkdir(run_dir)
-    runner = getattr(runners, runner_args['type'])(**runner_args)
-    result = runner.single_code_run(params_from_sampler, run_dir)
-    return result, params_from_sampler
-
-
-class DaskExecutor:
+class DaskExecutor(Executor):
     def __init__(
             self, sampler, runner_args, base_run_dir: str, worker_args: dict,
             num_workers: int):
@@ -42,38 +31,3 @@ class DaskExecutor:
         self.cluster.scale(self.num_workers)
         self.client = Client(self.cluster)
         print('Finished Setup')
-
-    def start_runs(self):
-        print(100*'=')
-        print('Starting Database generation')
-        print('Creating initial runs')
-        futures = []
-
-        # TODO: implement get_initial_parameters() from sampler
-        for _ in range(self.sampler.num_initial_points):
-            params = self.sampler.get_next_parameter()
-            new_future = self.client.submit(
-                run_simulation_task, self.runner_args, params,
-                self.base_run_dir)
-            futures.append(new_future)
-
-        print('Starting search')
-        seq = as_completed(futures)
-        completed = 0
-        for future in seq:
-            res = future.result()
-            completed += 1
-            print(res, completed)
-            # TODO: is this waiting for an open node or are we just
-            # pushing to queue?
-            if self.max_samples > completed:
-                # TODO: pass the previous result and parameters..
-                # For active learning
-                params = self.sampler.get_next_parameter()
-                if params is None:  # This is hacky
-                    continue
-                else:
-                    new_future = self.client.submit(
-                        run_simulation_task, self.runner_args,
-                        params, self.base_run_dir)
-                    seq.add(new_future)
--- src/executors/LocalDaskExecutor.py
+++ src/executors/LocalDaskExecutor.py
@@ -1,23 +1,11 @@
 # executors/DaskExecutor.py
 
 import os
-from dask.distributed import Client, as_completed
-# from dask_jobqueue import SLURMCluster
-import uuid
-import runners
-# from dask import delayed, compute
+from dask.distributed import Client
+from .base import Executor
 
 
-def run_simulation_task(runner_args, params_from_sampler, base_run_dir):
-    print("Making Run dir")
-    run_dir = os.path.join(base_run_dir, str(uuid.uuid4()))
-    os.mkdir(run_dir)
-    runner = getattr(runners, runner_args['type'])(**runner_args)
-    result = runner.single_code_run(params_from_sampler, run_dir)
-    return result, params_from_sampler
-
-
-class LocalDaskExecutor:
+class LocalDaskExecutor(Executor):
     def __init__(
             self, sampler, runner_args, base_run_dir: str, worker_args: dict,
             num_workers: int):
@@ -38,34 +26,3 @@ class LocalDaskExecutor:
         # n_workers=2, threads_per_worker=4
         self.client = Client()
         print('Finished Setup')
-
-    def start_runs(self):
-        print(100*'=')
-        print('Starting Database generation')
-        print('Creating initial runs')
-        futures = []
-
-        # TODO: implement get_initial_parameters() from sampler
-        for _ in range(5):
-            params = self.sampler.get_next_parameter()
-            new_future = self.client.submit(
-                run_simulation_task, self.runner_args, params,
-                self.base_run_dir)
-            futures.append(new_future)
-
-        print('Starting search')
-        seq = as_completed(futures)
-        completed = 0
-        for future in seq:
-            res = future.result()
-            completed += 1
-            print(res, completed)
-            # TODO: is this waiting for an open node or are we just pushing to
-            #  queue?
-            if self.max_samples > completed:
-                # TODO: pass the previous result and parameters..
-                params = self.sampler.get_next_parameter()
-                new_future = self.client.submit(
-                    run_simulation_task, self.runner_args, params,
-                    self.base_run_dir)
-                seq.add(new_future)
