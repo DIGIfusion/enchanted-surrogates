1ecfba2bf6c0c3f218f3efbc355d89aa093bdec6
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-04-19 14:01:31 +0300

    Made the gene runner, still needs more work and testing.

---
+++ src/runners/GENErunner.py
@@ -0,0 +1,35 @@
+# import numpy as np
+from .base import Runner
+from parsers import GENEparser as geneparser
+import subprocess
+import os
+
+
+class GENErunner(Runner):
+    def __init__(self, executable_path, is_gene_scan: bool, *args, **kwargs):
+        if is_gene_scan:
+            self.parser = geneparser()
+        else:
+            self.parser = gene_scan_parser()
+        self.executable_dir = executable_path
+    def single_code_run(self, params: dict, run_dir):
+        """Logic to run GENE"""
+        print(params)
+        
+        # write input file
+        self.parser.write_input_file(params, run_dir)
+        # run code #need to find a way to specify that the parameters file is not in the executable directory but the run directory
+        # maybe os.chdir allows the executable to be ran as if in run_dir
+        os.chdir(run_dir)
+        subprocess.call([f"{self.executable_path}"])
+
+        # process output
+        # self.parser.read_output_file(run_dir)
+
+        return True
+
+if __name__ == '__main__':
+    runner = GENErunner()
+    params = {'box-kymin':100.1, '_grp_species_0-omt': 2.75, '_grp_species_1-omt':5.1}
+    run_dir = 'path/to/run_dir' #The run directory is where the worker writes the parameters files and the results.
+    runner.single_code_run(params, run_dir)
