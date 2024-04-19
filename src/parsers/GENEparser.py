667e8936ce3e360be4dc612f3c026394a2d296c4
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-04-19 14:03:45 +0300

    starting changes to parser to include the ability to generate gene scan parameters files

--- src/parsers/GENEparser.py
+++ src/parsers/GENEparser.py
@@ -6,6 +6,7 @@ import f90nml
 from typing import List
 from copy import deepcopy
 
+
 class GENEparser(Parser):
     """An I/O parser for GENE
 
@@ -37,41 +38,12 @@ class GENEparser(Parser):
         """
         self.base_namelist = f90nml.read(base_params_dir) #odict_keys(['parallelization', 'box', 'in_out', 'general', 'geometry', '_grp_species_0', '_grp_species_1', 'units'])
         
-        
-    def write_input_file(self, params: dict, run_dir):
-        """
-        Write the GENE input file to the run directory specified. 
-        
-        Parameters
-        ----------
-            params (dict): The keys store strings of the names of the parameters as specified in the enchanted surrogates *_config.yaml configuration file.
-            The values stores floats of the parameter values to be ran in GENE.
+class GENE_scan_parser(GENEparser):
 
-            run_dir (string or path): A directory in the machine intended for the run of GENE.  
+class GENE_singles_parser(GENEparser)
 
-        """
-        params_keys = list(params.keys())
-        params_values = list(params.values())
-        patch = {}
-
-        for key, value in zip(params_keys,params_values):
-            group_name, variable_name = key.split('-')
-            if list(patch.keys()).count(group_name) > 0:
-                patch[group_name][variable_name] = value
-            else: patch[group_name] = {variable_name:value}
-
-        namelist = self.base_namelist
-        patch = f90nml.namelist.Namelist(patch)
-        namelist.patch(patch)
-        
-        f90nml.write(namelist, os.path.join(run_dir,'parameters'))
-
-    # what is returned here is returned to the runner for a single code run, which goes though the base executor to get to the future 
-    def read_output_file(self, run_dir: str):
-        raise NotImplementedError
-    
 if __name__ == '__main__':
     bounds = [[0.1, 300],[2,3.5],[4,6.8]]
-    params = {'box-kymin':100.1, '_grp_species_0-omt': 2.75, '_grp_species_1-omt':5.1, '_grp_species_1-dens':9000}
+    params = {'box-kymin':100.1, '_grp_species_0-omt': 2.75, '_grp_species_1-omt':5.1}
     parser = GENEparser(base_params_dir='/home/djdaniel/Downloads/parameters')
     parser.write_input_file(params,run_dir=os.getcwd())
