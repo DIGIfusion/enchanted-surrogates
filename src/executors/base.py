54eab5f7eba05d7cab671c36d86fb8b1f5c4a98a
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-04-29 13:09:23 +0300

    the base.py wasn't included in last commit by mistake

--- src/executors/base.py
+++ src/executors/base.py
@@ -3,7 +3,6 @@ import os
 import shutil
 from abc import ABC
 import uuid
-from dask.distributed import as_completed
 import runners
 
 def run_simulation_task(runner_args, params_from_sampler, base_run_dir):
@@ -33,39 +32,5 @@ class Executor(ABC):
 
         shutil.copyfile(config_filepath, os.path.join(self.base_run_dir, "CONFIG.yaml"))
 
-    def start_runs(self):
-        print(100 * "=")
-        print("Starting Database generation")
-        print("Creating initial runs")
-        futures = []
-
-        # TODO: implement get_initial_parameters() from sampler
-        for _ in range(self.sampler.num_initial_points):
-            params = self.sampler.get_next_parameter()
-            new_future = self.client.submit(
-                run_simulation_task, self.runner_args, params, self.base_run_dir
-            )
-            futures.append(new_future)
-
-        print("Starting search")
-        seq = as_completed(futures)
-        completed = 0
-        for future in seq:
-            res = future.result()
-            completed += 1
-            print(res, completed)
-            # TODO: is this waiting for an open node or are we just
-            # pushing to queue?
-            if self.max_samples > completed:
-                # TODO: pass the previous result and parameters..
-                # For active learning
-                params = self.sampler.get_next_parameter()
-                if params is None:  # This is hacky
-                    continue
-                else:
-                    new_future = self.client.submit(
-                        run_simulation_task, self.runner_args, params, self.base_run_dir
-                    )
-                    seq.add(new_future)
-
+    
     
