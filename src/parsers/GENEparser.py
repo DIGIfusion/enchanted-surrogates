41f2ebe8c60af6d064e32a3ee54ea3f90f229892
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-04-30 15:30:01 +0300

    I added a tested version of the gene scan parser. It take a dict of parameters where the values have an iterable of the values for each point to be scanned

--- src/parsers/GENEparser.py
+++ src/parsers/GENEparser.py
@@ -85,38 +85,106 @@ class GENE_single_parser(GENEparser):
         raise NotImplementedError
     
 class GENE_scan_parser(GENEparser): 
-    def write_input_file(self, params: dict, run_dir):
+    def write_input_file(self, params: dict, run_dir, file_name='parameters'):
 
+        # checking run dir exists and making Path for scan file
         print('Writing to', run_dir)
         if os.path.exists(run_dir):
-            input_fpath = os.path.join(run_dir, 'parameters')
+            input_fpath = os.path.join(run_dir, file_name)
         else:
             raise FileNotFoundError(f'Couldnt find {run_dir}')
-
-        params_keys = list(params.keys())
-        params_values = list(params.values())
-        params_scan_strings = []
-        patch = {}
-
-
-
-        for key, value in zip(params_keys,params_values):
-            group_name, variable_name = key.split('-')
-            if list(patch.keys()).count(group_name) > 0:
-                patch[group_name][variable_name] = value
-            else: patch[group_name] = {variable_name:value}
-
         namelist = self.base_namelist
-        patch = f90nml.namelist.Namelist(patch)
-        namelist.patch(patch)
+        namelist_string=str(namelist)
+    
+        def find_nth_occurrence(string, sub_string, n):
+            start_index = string.find(sub_string)
+            while start_index >= 0 and n > 1:
+                start_index = string.find(sub_string, start_index + 1)
+                n -= 1
+            return start_index
+
+        # finds the string location at the end of the line for a variable, just before \n
+        def var_end_loc(namelist_string: str, param_key):
+            group_name, var_name = param_key.split('-')
+            # print('GROUP NAME',group_name,'VAR NAME',var_name)
+            group_ordinal = 0 #0 is the 1st
+            if len(group_name.split('_'))>1:
+                # print('MORE THAN ONE GROUP OF SAME NAME')
+                _, _, group_name, group_ordinal = group_name.split('_')
+                group_ordinal = int(group_ordinal)+1
+
+            # print('GROUP NAME',group_name,'VAR NAME',var_name, 'ORDIANL',group_ordinal)
+
+            group_start = find_nth_occurrence(namelist_string, group_name, group_ordinal)
+            group_end = group_start+namelist_string[group_start:].find(f'/')
+            # print('GROUP',namelist_string[group_start:group_end])
+
+            var_start = group_start+namelist_string[group_start:group_end].find(var_name)
+            var_end = var_start+namelist_string[var_start:group_end].find("\n")
+            # print('VARLOC',var_start,var_end)
+            # print('VAR',namelist_string[var_start:var_end])
+            # print('START',namelist_string[var_start],'END',namelist_string[var_end])
+            return var_end
+
+        # Making kymin scanlist
+        kymin = params['box-kymin']
+        scanlist = f'      !scanlist: {kymin[0]}'
+        for ky in kymin[1:]:
+            scanlist += f', {ky}'
+        var_end = var_end_loc(namelist_string, 'box-kymin')
+        namelist_string = namelist_string[:var_end] + scanlist + namelist_string[var_end:]
+        #----------------------
         
-        f90nml.write(namelist, input_fpath)
+        def var_ordinal(namelist,var_name):
+            first_var_loc = 0
+            for g in namelist.groups():
+                #e.g. print(g) --> (('parallelization', 'n_procs_x'), 1)
+                if g[0][1]==var_name:
+                    break        
+                else:
+                    first_var_loc += 1
+
+            # print(f'FIRST {var_name} LOCATION: it is the,{first_var_loc}th variable in the parameters file.')
+            return first_var_loc
+
+        def make_scanwith(namelist, var_name, values):
+            kymin_loc = var_ordinal(namelist, 'kymin')
+            # print(f'Generating scanwith string for {var_name}')
+            
+            # print('VALUES',values)
+            scanwith = f'       !scanwith: {kymin_loc}, {values[0]}'
+            for omn in values[1:]:
+                scanwith += f', {omn}'
+            return scanwith
+
+        scanwith = {}
+        params.pop('box-kymin')
+        for var_name in params.keys():
+            scanwith[var_name] = make_scanwith(namelist, var_name, values=params[var_name])
+        # print('SCANWITH',scanwith)
+            # finds the string location at the end of the line for a variable, just before \n
+        
+        
+        for param_key in list(params.keys()):
+            var_end = var_end_loc(namelist_string,param_key)
+            namelist_string = namelist_string[:var_end] + scanwith[param_key] + namelist_string[var_end:]
+        # print(namelist_string)        
+        
+        #Writing the final namelist stirng to file. This is the scan parameters file.
+        with open(input_fpath, 'w') as file:
+            file.write(namelist_string)        
 
     def read_output_file(self, run_dir: str):
         raise NotImplementedError
     
 if __name__ == '__main__':
     bounds = [[0.1, 300],[2,3.5],[4,6.8]]
-    params = {'box-kymin':100.1, '_grp_species_0-omt': 2.75, '_grp_species_1-omt':5.1}
-    parser = GENE_scan_parser(base_params_dir='/home/djdaniel/Downloads/parameters')
-    parser.write_input_file(params,run_dir=os.getcwd())
+    # params = {'box-kymin':100.1, '_grp_species_0-omt': 2.75, '_grp_species_1-omt':5.1}
+    generator = np.random.default_rng(seed=238476592)
+    omn = generator.uniform(5,60,5)
+    params = {'box-kymin':generator.uniform(0.05,1,5),
+          '_grp_species_0-omn':omn,
+          '_grp_species_1-omn':omn,
+          '_grp_species_1-omt':generator.uniform(10,70,5)}
+    parser = GENE_scan_parser(base_params_dir = os.path.join(os.getcwd(),'src','parsers','parameters_base'))
+    parser.write_input_file(params,run_dir=os.getcwd(),file_name='parameters_scanwith')
