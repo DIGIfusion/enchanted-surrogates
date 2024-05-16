95347ea9db5d0e6f56a8cd5d2b954ffc471b64f3
lorenzozanisi <lorenzo.zanisi@ukaea.uk>
2024-05-16 10:18:16 +0100

    adding statipoolrunner

---
+++ src/runners/STATICPOOLrunner.py
@@ -0,0 +1,10 @@
+from .base import Runner
+
+class STATICPOOLrunner(Runner): 
+    def __init__(self, *args, **kwargs): 
+        """ This is a dummy class as for the moment the sampler handles everything"""
+        pass 
+
+    def single_code_run(self, params: dict, run_dir: str):
+        pass 
+        return None
