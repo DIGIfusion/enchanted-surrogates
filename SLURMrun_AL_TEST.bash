d29301bfdb95946f9ee83fc4068fee02322075b7
Adam Kit <adam.kit@helsinki.fi>
2024-05-17 17:19:33 +0300

    aforementioned paths found in SLURMrun_AL_TEST.bash

---
+++ SLURMrun_AL_TEST.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Call with `sbatch SLURMrun.bash` and modify below with your relevant SLURM config
+#SBATCH --job-name=SURROGATE_WORKFLOW
+#SBATCH --account=project_2007159
+#SBATCH --time=6:00:00
+#SBATCH --partition=interactive
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --out=./run.out
+
+module purge 
+module load python-data                          # changes based on CLUSTER 
+# module load pytorch
+source .venv/bin/activate                      # changes based on CLUSTER 
+
+current_dir=$(pwd)
+export PYTHONPATH=$PYTHONPATH:$current_dir/src   # does not change!
+export PYTHONPATH=$PYTHONPATH:$current_dir/bmdal/bmdal_reg
+
+config_file=AL_example.yaml                     # changes based on USE CASE
+
+echo $config_file                                # does not change!
+python3 src/run.py -cf=$current_dir/configs/$config_file
