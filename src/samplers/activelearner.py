6d3bcabcce58cd754080792dec386541e8086373
Amanda Bruncrona <amanda_bruncrona@hotmail.com>
2024-05-24 16:51:45 +0300

    Docstrings for samplers

--- src/samplers/activelearner.py
+++ src/samplers/activelearner.py
@@ -1,35 +1,75 @@
 """
 samplers/activelearner.py
 
-As per the executor, and in addition to the and get_initial_parameters(), 
-active learners samplers need the following methods: 
-- collect_batch_results(outputs) 
+As per the executor, and in addition to the and get_initial_parameters(),
+active learners samplers need the following methods:
+- collect_batch_results(outputs)
 
 If using active learning with your code, your parser must also have:
-- Parameters: 
-    - train, valid, test 
-- Methods: 
-    - update_pool_and_train(outputs) -> None 
+- Parameters:
+    - train, valid, test
+- Methods:
+    - update_pool_and_train(outputs) -> None
         - Updates the pool and train attributes
     - get_train_valid_datasplit() -> (x_train, y_train), (x_valid, y_valid)
-    - 
+    -
 """
-try: 
+
+try:
     from bmdal_reg.bmdal.algorithms import select_batch
     from bmdal_reg.bmdal.feature_data import TensorFeatureData
 except ImportError as e:
-    print('Cannot find bmdal_reg, please point to those in your bash script if you intend to use ActiveLearnerBMDAL')
+    print(
+        "Cannot find bmdal_reg.",
+        "Please point to those in your bash script if you intend to use ActiveLearnerBMDAL",
+        str(e),
+    )
 import os
 import torch
 import numpy as np
 from common import S
-import parsers 
+import parsers
 from .base import Sampler
 
 
 class ActiveLearnerBMDAL(Sampler):
+    """
+    Active Learner using BMDAL library for Active Learning.
+
+    Args:
+        total_budget (int): Total budget for the active learning process.
+        parser_kwargs (dict): Keyword arguments for the parser.
+        model_kwargs (dict): Keyword arguments for the model.
+        train_kwargs (dict): Keyword arguments for training.
+
+    Attributes:
+        total_budget (int): Total budget for the active learning process.
+        bounds (list): Bounds for the parameter space.
+        parameters (list): Names of the parameters.
+        aquisition_batch_size (int): Batch size for acquisition.
+        init_num_samples (int): Number of initial samples.
+        selection_method (str): Method for selecting samples.
+        kernel_transform (list): Kernel transformation methods.
+        base_kernel (str): Base kernel for BMDAL.
+        parser_kwargs (dict): Keyword arguments for the parser.
+        model_kwargs (dict): Keyword arguments for the model.
+        train_kwargs (dict): Keyword arguments for training.
+        metrics (list): List to store metrics.
+    """
+
     sampler_interface = S.ACTIVE
-    def __init__(self, total_budget: int, parser_kwargs, model_kwargs, train_kwargs, *args, **kwargs): 
+
+    def __init__(
+        self,
+        total_budget: int,
+        parser_kwargs,
+        model_kwargs,
+        train_kwargs,
+        *args,
+        **kwargs,
+    ):
+
+        # fmt: off
         self.total_budget          = total_budget
         self.bounds                = kwargs.get('bounds', [None])
         self.parameters            = kwargs.get('parameters', ['NA']*len(self.bounds))
@@ -45,101 +85,234 @@ class ActiveLearnerBMDAL(Sampler):
         self.metrics = []
 
         self.parser                = getattr(parsers, parser_kwargs.pop('type'))(**parser_kwargs)
+        # fmt: on
 
     def get_initial_parameters(self):
+        """
+        Generates initial parameter samples.
+
+        Returns:
+            list: List of dictionaries containing initial parameter samples.
+
+        """
         # TODO: more than just random samples
-        assert self.parameters[0] != 'NA'
+        assert self.parameters[0] != "NA"
         batch_samples = []
         for _ in range(self.init_num_samples):
-            params = [torch.distributions.Uniform(lb, ub).sample().item() for (lb, ub) in self.bounds]
+            params = [
+                torch.distributions.Uniform(lb, ub).sample().item()
+                for (lb, ub) in self.bounds
+            ]
             param_dict = dict(zip(self.parameters, params))
             batch_samples.append(param_dict)
         return batch_samples
 
     def _get_new_idxs_from_pool(self, model, train, pool) -> torch.Tensor:
+        """
+        Selects new indices from the pool using BMDAL's select_batch function.
+
+        Args:
+            model: PyTorch model.
+            train: Training data.
+            pool: Pool data.
+
+        Returns:
+            torch.Tensor: Tensor containing new indices.
+
+        """
         y_train = train[:, self.parser.output_col_idxs].float()
         train = train[:, self.parser.input_col_idxs].float()
         pool = pool[:, self.parser.input_col_idxs].float()
         train_data = TensorFeatureData(train)
         pool_data = TensorFeatureData(pool)
 
-        new_idxs, _ = select_batch(batch_size=self.aquisition_batch_size, models=[model],
-                           data={'train': train_data, 'pool': pool_data}, y_train=y_train,
-                           selection_method=self.selection_method, sel_with_train=True,
-                           base_kernel=self.base_kernel, kernel_transforms=self.kernel_transform)
+        new_idxs, _ = select_batch(
+            batch_size=self.aquisition_batch_size,
+            models=[model],
+            data={"train": train_data, "pool": pool_data},
+            y_train=y_train,
+            selection_method=self.selection_method,
+            sel_with_train=True,
+            base_kernel=self.base_kernel,
+            kernel_transforms=self.kernel_transform,
+        )
 
         return new_idxs
 
-    def get_next_parameter(self, model: torch.nn.Module, *args, **kwargs) -> list[dict[str, float]]:
-        train, pool = self.parser.train, self.parser.pool
-        new_idxs = self._get_new_idxs_from_pool(model, train,pool)
+    def get_next_parameter(
+        self,
+        model: torch.nn.Module,
+        train: torch.Tensor,
+        pool: torch.Tensor,
+        *args,
+        **kwargs,
+    ) -> list[dict[str, float]]:
+        """
+        Generates the next parameter samples based on the model predictions.
+
+        Args:
+            model (torch.nn.Module): PyTorch model.
+
+        Returns:
+            list: List of dictionaries containing next parameter samples.
+
+        """
+        # train, pool = self.parser.train, self.parser.pool
+        new_idxs = self._get_new_idxs_from_pool(model, train, pool)
         results = []
         for idx in new_idxs:
-            results.append({'input': self.parser.pool[idx, self.parser.input_col_idxs], 'output': None, 'pool_idxs':idx}) 
+            results.append(
+                {
+                    "input": self.parser.pool[idx, self.parser.input_col_idxs],
+                    "output": None,
+                    "pool_idxs": idx,
+                }
+            )
         # TODO: convert indicies to unscaled parameters
         return results
 
     def collect_batch_results(self, results: list[dict[str, dict]]) -> torch.Tensor:
+        """
+        Collects results from the batch.
+
+        Args:
+            results (list): List of dictionaries containing batch results.
+
+        Returns:
+            torch.Tensor: Tensor containing batch results.
+
+        """
         outputs_as_tensor = torch.empty(len(results), len(self.parser.keep_keys))
         for n, result in enumerate(results):
-            x = result['inputs']
-            y = result['output']
+            x = result["inputs"]
+            y = result["output"]
             outputs_as_tensor[n] = torch.cat((x, y))
         return outputs_as_tensor
 
     def update_pool_and_train(self):
+        """
+        Updates the pool and train attributes.
+
+        """
         raise NotImplementedError()
-    
+
     def update_metrics(self, metrics: dict) -> None:
-        """ Metrics come from the training run """
-        r2_score =  metrics['val_r2_losses']
+        """
+        Updates metrics based on training results.
+
+        Args:
+            metrics (dict): Dictionary containing metrics.
+
+        """
+        r2_score = metrics["val_r2_losses"]
         best_r2_score = max(r2_score)
         self.metrics.append(best_r2_score)
 
 
 class ActiveLearningBMDALStaticPoolSampler(ActiveLearnerBMDAL):
     """
-    Loads a database of runs that have already been collected. 
+    Active Learning BMDAL Static Pool Sampler.
+
+    Loads a database of runs that have already been collected.
     This is useful for testing various acquisition strategies
     in Active Learning on fixed datasets. Uses Pandas under the hood.
     """
+
     sampler_interface = S.ACTIVEDB
+
     def __init__(self, **kwargs):
         """
+        Initializes the Active Learning BMDAL Static Pool Sampler.
+
         Args:
             data_path (str): The path where the data is stored. Accepted formats: csv, pkl, h5.
         """
         super().__init__(**kwargs)
 
     def collect_batch_results(self, results: list[dict[str, dict]]) -> torch.Tensor:
-        """ ignores outputs as this is labeled static pool based """
-        idxs_as_tensor = torch.tensor([res['input']['pool_idxs'] for res in results])
+        """
+        Collects results, ignoring outputs as this is labeled static pool-based.
+
+        Args:
+            results (list): List of dictionaries containing batch results.
+
+        Returns:
+            torch.Tensor: Tensor containing batch results.
+
+        """
+        idxs_as_tensor = torch.tensor([res["input"]["pool_idxs"] for res in results])
         return idxs_as_tensor
-    
+
     def get_initial_parameters(self) -> list[dict]:
-        """ Returns a subset of initial parameters """
+        """
+        Returns a subset of initial parameters.
+
+        Returns:
+            list: List of dictionaries containing initial parameter samples.
+
+        """
         params = []
         for _ in range(self.init_num_samples):
-            idxs   = np.random.randint(0, len(self.parser.pool))
-            result = {'input': self.parser.pool[idxs, self.parser.input_col_idxs], 'output': self.parser.pool[idxs, self.parser.output_col_idxs], 'pool_idxs':idxs}
+            idxs = np.random.randint(0, len(self.parser.pool))
+            result = {
+                "input": self.parser.pool[idxs, self.parser.input_col_idxs],
+                "output": self.parser.pool[idxs, self.parser.output_col_idxs],
+                "pool_idxs": idxs,
+            }
             params.append(result)
         return params
 
-    def get_next_parameter(self, model: torch.nn.Module) -> list[dict[str, float]]:
-        idxs    = self._get_new_idxs_from_pool(model, self.parser.train, self.parser.pool)
+    def get_next_parameter(
+        self, model: torch.nn.Module, train: torch.Tensor, pool: torch.Tensor
+    ) -> list[dict[str, float]]:
+        """
+        Generates the next parameter samples based on the model predictions.
+
+        Args:
+            model (torch.nn.Module): PyTorch model.
+
+        Returns:
+            list: List of dictionaries containing next parameter samples.
+
+        """
+        idxs = self._get_new_idxs_from_pool(model, train, pool)
         results = []
-        for idx in idxs: 
-            results.append({'input': self.parser.pool[idx, self.parser.input_col_idxs], 'output': self.parser.pool[idx, self.parser.output_col_idxs], 'pool_idxs':idx}) 
+        for idx in idxs:
+            results.append(
+                {
+                    "input": self.parser.pool[idx, self.parser.input_col_idxs],
+                    "output": self.parser.pool[idx, self.parser.output_col_idxs],
+                    "pool_idxs": idx,
+                }
+            )
         return results
 
-    def dump_results(self, base_run_dir) -> None:
-        print(100*'=')
-        print(f'Final Results for {self.selection_method}')
+    def dump_results(self, base_run_dir: str) -> None:
+        """
+        Prints and saves final datasets to disk.
 
-        print(self.metrics)
-        print(f'Saving final datasets to disk: {base_run_dir}')
-        for name, dset in zip(['train', 'valid', 'test'], [self.parser.train, self.parser.valid, self.parser.test]): 
-            fpath = os.path.join(base_run_dir, f'{name}.pt')
+        Args:
+            base_run_dir: Base directory path for saving.
+
+        """
+        print(100 * "=")
+        print(f"Final Results for {self.selection_method}")
+
+        metric_fname = os.path.join(base_run_dir, "final_metrics.txt")
+        self.write_metrics_to_file(metric_fname, self.metrics)
+        print(f"Saving final datasets to disk: {base_run_dir}")
+        for name, dset in zip(
+            ["train", "valid", "test"],
+            [self.parser.train, self.parser.valid, self.parser.test],
+        ):
+            fpath = os.path.join(base_run_dir, f"{name}.pt")
             torch.save(dset, fpath)
 
-        
+    def write_metrics_to_file(self, filename: str, metrics: list[float]):
+        # TODO: establish good metrics
+        with open(filename, "w") as file:
+            header = "STEP_NUM, R2\n"
+            file.write(header)
+            for n_al_step in range(len(metrics)):
+                write_string = f"{n_al_step}, {metrics[n_al_step]}\n"
+                file.write(write_string)
--- src/samplers/base.py
+++ src/samplers/base.py
@@ -6,18 +6,50 @@ from common import S
 
 
 class Sampler(ABC):
-    """ Parameter Space Samplers """
+    """
+    Abstract base class for parameter space samplers.
+
+    Attributes:
+        sampler_interface (S): The type of sampler interface.
+    """
+
     # TODO: param_dict = {key: value for key, value in zip(self.parameters, params)} globally
 
     @abstractmethod
-    def get_next_parameter(self, *args, **kwargs) -> Union[list[dict[str, float]], dict[str, float]]:
-        raise NotImplementedError('Sampling not implemented yet')
+    def get_next_parameter(
+        self, *args, **kwargs
+    ) -> Union[list[dict[str, float]], dict[str, float]]:
+        """
+        Abstract method for getting the next parameter set.
 
-    @property 
+        Args:
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            Union[list[dict[str, float]], dict[str, float]]: The next parameter set.
+        """
+        raise NotImplementedError("Sampling not implemented yet")
+
+    @property
     @abstractmethod
-    def sampler_interface(self) -> S: 
-        raise NotImplementedError('sampler_interface is not set')
+    def sampler_interface(self) -> S:
+        """
+        Abstract property for getting the sampler interface type.
 
-    @abstractmethod 
-    def get_initial_parameters(self,) -> list[dict[str, float]]: 
-        raise NotImplementedError('Getting initial parameters is not implemented yet!')
+        Returns:
+            S: The sampler interface type.
+        """
+        raise NotImplementedError("sampler_interface is not set")
+
+    @abstractmethod
+    def get_initial_parameters(
+        self,
+    ) -> list[dict[str, float]]:
+        """
+        Abstract method for getting the initial parameters.
+
+        Returns:
+            list[dict[str, float]]: The initial parameters.
+        """
+        raise NotImplementedError("Getting initial parameters is not implemented yet!")
