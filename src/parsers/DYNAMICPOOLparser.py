4722cbb4c055096430df1ab72ec51688c94756ed
Adam Kit <adam.kit@helsinki.fi>
2024-05-24 17:10:03 +0300

    active learning gpu cpu works somehow

--- src/parsers/DYNAMICPOOLparser.py
+++
@@ -1,8 +0,0 @@
-#TODO implement for having arbitrary codes in the loop
-# this will essentially be subclass of other parsers 
-# but with collect_batch 
-# and update_train_pool
-# see loop in executor for methods to add
-
-def update_pool_and_train(self, outputs):
-    self.train = torch.concatenate((self.train, self.pool))
