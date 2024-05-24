84eebe412645ae55db0fb1ccd2a46b51f5cc9772
Adam Kit <adam.kit@helsinki.fi>
2024-05-24 17:26:09 +0300

    Update config files and typesetting for common utils

--- src/common/interfaces.py
+++ src/common/interfaces.py
@@ -1,11 +1,13 @@
 from enum import Enum
 
+
 class S(Enum):
     # Samplers ENUM
-    SEQUENTIAL=0
-    BATCH=1
-    ACTIVE=2
-    ACTIVEDB=3
+    SEQUENTIAL = 0
+    BATCH = 1
+    ACTIVE = 2
+    ACTIVEDB = 3
+
 
 """
 
@@ -15,8 +17,8 @@ def model_interface(self) -> List[M]:
     # A list for storing what model interfaces the training method is compatible with
     raise NotImplementedError("model_interface not set!")
 
-@property 
+@property
 @abstractmethod
-def sampler_interface(self) -> S: 
+def sampler_interface(self) -> S:
     raise NotImplementedError('sampler_interface is not set')
 """
