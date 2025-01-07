d8f4b8880c86ded8ce7bb5843793a18d9ff5518f
AaroJarvinen <aarojarvinen@vtt.fi>
2025-01-07 10:36:35 +0200

    HELENA BETA ITERATOR

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -1,5 +1,6 @@
 from .TGLFrunner import TGLFrunner
 from .SIMPLErunner import SIMPLErunner
 from .HELENArunner import HELENArunner
+from .HELENArunner_beta  import HELENArunner_beta
 from .MISHKArunner import MISHKArunner
 from .STATICPOOLrunner import STATICPOOLrunner
