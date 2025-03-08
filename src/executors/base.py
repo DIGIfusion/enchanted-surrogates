338695cb456c6363cbd4cd92426929e1ea7fb727
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-03-08 16:30:37 +0200

    update

--- src/executors/base.py
+++ src/executors/base.py
@@ -55,6 +55,7 @@ def run_simulation_task(
         print("="*100,f"\nThere was a Python error on a DASK worker when running a simulation task:\n{exc}", flush=True)
         #print the whole traceback and not just the last error
         print(traceback.format_exc())
+        result = None
     return result
 
 
