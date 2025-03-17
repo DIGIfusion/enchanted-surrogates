7ffd08718469d11ba8d24ae8a08034268eb93c88
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-03-17 14:00:02 +0200

    The partition for this one can be small

--- SLURMrun_lumi.bash
+++ SLURMrun_lumi.bash
@@ -3,19 +3,23 @@
 #SBATCH --job-name=SURROGATE_WORKFLOW
 #SBATCH --account=project_462000451
 #SBATCH --time=00:30:00
-#SBATCH --partition=interactive
+#SBATCH --partition=small
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --out=./run.out
 
+echo PURGING MODULES
 module purge 
+
+echo LOADING PYTHON
 export PATH="/project/project_462000451/enchanted_container_lumi3/bin:$PATH"                          # changes based on CLUSTER 
 # source .venv/bin/activate                      # changes based on CLUSTER 
 
+echo ADDING SRC TO PYTHON PATH
 current_dir=$(pwd)
 export PYTHONPATH=$PYTHONPATH:$current_dir/src   # does not change!
 
-config_file=simple_config_lumi.yaml                     # changes based on USE CASE
+config_file=simple_config_lumi_simulation.yaml                     # changes based on USE CASE
 
-echo $config_file                                # does not change!
+echo RUNNING ENCHANTED SURROGATES WITH CONFIG FILE: $current_dir/configs/$config_file  # does not change!
 python3 src/run.py -cf=$current_dir/configs/$config_file
