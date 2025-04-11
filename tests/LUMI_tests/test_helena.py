a0f1ea35de09b4b5916687c9a79ec8acf7a7a7c9
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-04-11 17:13:09 +0300

    I deleted the gene_parser_tests since it was causing pylint errors and belongs on another branch

--- tests/LUMI_tests/test_helena.py
+++ tests/LUMI_tests/test_helena.py
@@ -6,7 +6,7 @@ def test_helena_pedestal_sample_betaN_iteration():
     config_path = os.path.join(os.path.dirname(__file__), 'configs','helena_pedestal_sample_betaN_iteration.yaml')
     
     args = run.load_configuration(config_path)
-    os.system(f'rm -r {args['executor']['base_run_dir']}/*')
+    os.system(f"rm -r {args['executor']['base_run_dir']}/*")
     run.main(args)
     
     
