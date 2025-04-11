e6d469cb4c5793c6d562f98d81ab429e9b2c097c
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-04-11 13:11:13 +0300

    I added src to python path in the run.py dir. This way it does not need to be handled before running it

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
 # run.py
 from dask.distributed import print
 print('PERFORMING IMPORTS')
+import os,sys
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','src'))
 import yaml
 
 import argparse
