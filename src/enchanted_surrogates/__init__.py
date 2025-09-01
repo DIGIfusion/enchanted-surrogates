836b47693db458ea7b49876929dfe2a136e178ec
Amanda <amanda_bruncrona@hotmail.com>
2025-09-01 17:12:57 +0300

    Update __init__.py

--- src/enchanted_surrogates/__init__.py
+++ src/enchanted_surrogates/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import entry_points
 
 
 def load_plugins():
+    """Load all plugins registered under the 'enchanted_surrogates' entry points."""
     eps = (list(entry_points(group="enchanted_surrogates.parsers"))
            + list(entry_points(group="enchanted_surrogates.runners")))
-    print(eps)
     return {ep.name: ep.load() for ep in eps}
