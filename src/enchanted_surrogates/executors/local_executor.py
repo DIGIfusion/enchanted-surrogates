6397fb02e8ae42161dbd6cdb57fe93d19738de34
Adam Kit <adam.kit@vtt.fi>
2025-09-01 15:41:00 +0300

    update develop with the joblib executor

--- src/enchanted_surrogates/executors/local_executor.py
+++ src/enchanted_surrogates/executors/local_executor.py
@@ -9,7 +9,7 @@ class LocalExecutor(Executor):
     def start_runs(self):
         while self.sampler.has_budget: 
             samples: list[dict] = self.sampler.get_next_samples()
-
+            
             for sample in samples: 
                 sample_run_dir = os.path.join(self.base_run_dir, str(uuid.uuid4()))  # TODO. uuid.uuid should probably have a random seed ? 
                 new_future     = run_simulation_task(self.runner_args, sample_run_dir, params=sample)
--- tests/test_simple_workflow.py
+++ tests/test_simple_workflow.py
@@ -1,5 +1,5 @@
 import pytest 
-from enchanted_surrogates.executors import LocalExecutor
+from enchanted_surrogates.executors import LocalExecutor, JoblibExecutor
 from enchanted_surrogates.samplers.random_sampler import RandomSampler 
 import glob 
 import os 
@@ -34,4 +34,27 @@ def test_full_workflow_local(tmp_path):
     executor.clean() 
 
 
+def test_full_workflow_joblib(tmp_path): 
+    config = {}
+    # -- sampler 
+    # TODO: test different samplers
+    bounds = [[-5, 5], [0, 1]]
+    parameters = ['c1', 'c2']
+    total_budget = 50
+    sampler = RandomSampler(bounds, total_budget, parameters)
 
+    # -- runner args
+    runner_args = {
+        'type': 'ExampleRunner'
+    }
+
+    base_run_dir = tmp_path # f"{os.path.dirname(__file__)}/example"
+    # create the executor 
+    executor = JoblibExecutor(sampler=sampler, runner_args=runner_args, base_run_dir=base_run_dir, **config)
+    executor.start_runs() 
+    # This should create {total_budget} folders with ??? inside 
+
+    created_rundirs = glob.glob(os.path.join(base_run_dir, "*"))
+    assert len(created_rundirs) == total_budget
+    
+    executor.clean() 
