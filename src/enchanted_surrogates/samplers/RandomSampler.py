05a5b2765a5df56d6f9148889c3894451ff80210
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-09-02 22:37:09 +0300

    I needed to change random_sampler.py to RandomSampler.py so it matched its class name and can be imported with only one variable 'type' in the arguments. I got the dask executor working. It will also make a nice enchanted_dataset.csv file, sotres the worker logs and has and option to print the worker status and block until cluster starts for debugging purposes

---
+++ src/enchanted_surrogates/samplers/RandomSampler.py
@@ -0,0 +1,26 @@
+from .base_sampler import Sampler
+
+import numpy as np
+
+class RandomSampler(Sampler):
+    BATCH_SAMPLE_SIZE = 1
+    def __init__(self, bounds, total_budget, parameters, **kwargs):
+        self.budget = total_budget
+        self.bounds = bounds
+        self.parameters = parameters
+
+
+    def get_next_samples(self) -> list[dict]:
+        # TODO not use uniform?
+        # TODO batch samples
+        list_param_dicts = []
+        for _ in range(self.BATCH_SAMPLE_SIZE):
+            params = [np.random.uniform(low, high) for low, high in self.bounds]
+            param_dict = {key: value for key, value in zip(self.parameters, params)}
+            list_param_dicts.append(param_dict)
+        self.submitted += len(list_param_dicts)
+        return list_param_dicts
+
+    def register_future(self, future):
+        """ Doesn't matter for random sampler TODO: Probably? """
+        return None
