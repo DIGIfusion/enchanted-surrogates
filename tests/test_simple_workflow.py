5073f5478dc2f0c400a35d6025212a8d4915f494
Amanda <amanda_bruncrona@hotmail.com>
2025-09-05 09:44:35 +0300

    Ignore LUMI tests

--- tests/test_simple_workflow.py
+++ tests/test_simple_workflow.py
@@ -1,22 +1,23 @@
-import pytest 
 from enchanted_surrogates.executors import LocalExecutor, JoblibExecutor
 from enchanted_surrogates.samplers.random_sampler import RandomSampler
-import glob 
-import os 
+import glob
+import os
 import shutil
 
 
 """ 
 
 """
+
+
 # https://docs.pytest.org/en/stable/how-to/tmp_path.html
-def test_full_workflow_local(tmp_path): 
+def test_full_workflow_local(tmp_path):
     config = {}
-    # -- sampler 
+    # -- sampler
     # TODO: test different samplers
     bounds = [[-5, 5], [0, 1]]
     parameters = ['c1', 'c2']
-    total_budget = 50
+    total_budget = 10
     sampler = RandomSampler(bounds, total_budget, parameters)
 
     # -- runner args
@@ -24,10 +25,11 @@ def test_full_workflow_local(tmp_path):
         'type': 'ExampleRunner'
     }
 
-    base_run_dir = tmp_path # f"{os.path.dirname(__file__)}/example"
-    # create the executor 
+    base_run_dir = tmp_path  # f"{os.path.dirname(__file__)}/example"
+    # create the executor
 
-    executor = LocalExecutor(sampler=sampler, runner_args=runner_args, base_run_dir=base_run_dir, **config)
+    executor = LocalExecutor(
+        sampler=sampler, runner_args=runner_args, base_run_dir=base_run_dir, **config)
     executor.start_runs()
     # This should create {total_budget} folders with ??? inside
 
@@ -38,9 +40,9 @@ def test_full_workflow_local(tmp_path):
     shutil.rmtree(base_run_dir)
 
 
-def test_full_workflow_joblib(tmp_path): 
+def test_full_workflow_joblib(tmp_path):
     config = {}
-    # -- sampler 
+    # -- sampler
     # TODO: test different samplers
     bounds = [[-5, 5], [0, 1]]
     parameters = ['c1', 'c2']
@@ -52,14 +54,13 @@ def test_full_workflow_joblib(tmp_path):
         'type': 'ExampleRunner'
     }
 
-    base_run_dir = tmp_path # f"{os.path.dirname(__file__)}/example"
-    # create the executor 
-    executor = JoblibExecutor(sampler=sampler, runner_args=runner_args, base_run_dir=base_run_dir, **config)
+    base_run_dir = tmp_path  # f"{os.path.dirname(__file__)}/example"
+    # create the executor
+    executor = JoblibExecutor(
+        sampler=sampler, runner_args=runner_args, base_run_dir=base_run_dir, **config)
     executor.start_runs() 
-    # This should create {total_budget} folders with ??? inside 
+    # This should create {total_budget} folders with ??? inside
 
     created_rundirs = glob.glob(os.path.join(base_run_dir, "*"))
     assert len(created_rundirs) == total_budget
-    
-    executor.clean() 
-
+    executor.clean()
