26688488e1aacb3d21c5d64f97a55e4d2debb7e8
Amanda <amanda_bruncrona@hotmail.com>
2025-09-16 16:04:09 +0300

    Linting

--- src/enchanted_surrogates/runners/base_runner.py
+++ src/enchanted_surrogates/runners/base_runner.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
+
 class Runner(ABC):
-    def __init__(self, **kwargs): 
-        pass 
-    
+    def __init__(self, **kwargs):
+        pass
+
     @abstractmethod
     def single_code_run(self, run_dir: str, params: dict = None) -> dict:
         raise NotImplementedError("Subclasses must implement this method")
