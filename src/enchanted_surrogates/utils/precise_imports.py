7487b6c339340f73aa83495c35794c63fb6081c1
Amanda <amanda_bruncrona@hotmail.com>
2025-09-16 16:43:58 +0300

    Function defined twice

--- src/enchanted_surrogates/utils/precise_imports.py
+++ src/enchanted_surrogates/utils/precise_imports.py
@@ -1,9 +1,13 @@
+import os
 import re
 import importlib
-import os, sys
+import sys
 from enchanted_surrogates import load_plugins
-src_dir = os.path.dirname(os.sep.join(os.path.normpath(__file__).split(os.sep)[:__file__.split(os.sep).index("enchanted_surrogates") + 1]))
+src_dir = os.path.dirname(
+    os.sep.join(
+        os.path.normpath(__file__).split(os.sep)[:__file__.split(os.sep).index("enchanted_surrogates") + 1]))
 sys.path.append(src_dir)
+
 
 def detect_case_style(s):
     """
@@ -31,6 +35,7 @@ def detect_case_style(s):
     else:
         return "unknown"
 
+
 def snake_to_pascal(s):
     """
     Converts a snake_case string to PascalCase.
@@ -48,6 +53,7 @@ def snake_to_pascal(s):
 
     return ''.join(word.title() for word in s.split('_'))
 
+
 def camel_or_pascal_to_snake(s):
     """
     Converts a camelCase or PascalCase string to snake_case.
@@ -63,6 +69,7 @@ def camel_or_pascal_to_snake(s):
         The converted snake_case string.
     """
     return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
+
 
 def get_snake_and_pascal(string):
     """
@@ -91,8 +98,10 @@ def get_snake_and_pascal(string):
         string_snake = camel_or_pascal_to_snake(string)
         string_pascal = string
     else:
-        raise ValueError(f"Input string '{string}' must be in either snake_case or PascalCase format.")
+        raise ValueError(
+            f"Input string '{string}' must be in either snake_case or PascalCase format.")
     return string_snake, string_pascal
+
 
 def import_sampler(type, sampler_kwargs):
     """
@@ -122,8 +131,11 @@ def import_sampler(type, sampler_kwargs):
     if type_snake in eps:
         sampler = eps[type_snake](**sampler_kwargs)
     else:
-        sampler = getattr(importlib.import_module(f'enchanted_surrogates.samplers.{type_snake}'),type_pascal)(**sampler_kwargs)
+        sampler = getattr(
+            importlib.import_module(
+                f'enchanted_surrogates.samplers.{type_snake}'), type_pascal)(**sampler_kwargs)
     return sampler
+
 
 def import_runner(type, runner_kwargs):
     """
@@ -152,41 +164,12 @@ def import_runner(type, runner_kwargs):
     if type_snake in eps:
         runner = eps[type_snake](**runner_kwargs)
     else:
-        runner = getattr(importlib.import_module(f'enchanted_surrogates.runners.{type_snake}'),type_pascal)(**runner_kwargs)
+        runner = getattr(
+            importlib.import_module(
+                f'enchanted_surrogates.runners.{type_snake}'), type_pascal)(**runner_kwargs)
     return runner
 
-def import_executor(type, executor_kwargs):
-    """
-    Dynamically imports and instantiates an executor class based on naming convention.
 
-    Parameters
-    ----------
-    type : str
-        The name of the sampler (in snake_case or PascalCase).
-    sampler_kwargs : dict
-        Keyword arguments to pass to the sampler constructor.
-
-    Returns
-    -------
-    object
-        An instance of the sampler class.
-
-    Raises
-    ------
-    ImportError
-        If the module or class cannot be found.
-    """
-    type_snake, type_pascal = get_snake_and_pascal(type)
-    eps = load_plugins()
-
-    if type_snake in eps:
-        executor = eps[type_snake](**executor_kwargs)
-    else:
-        executor = getattr(importlib.import_module(f'enchanted_surrogates.executors.{type_snake}'),type_pascal)(**executor_kwargs)
-    return executor
-
-    
-    
 def import_executor(type, executor_kwargs):
     """
     Dynamically imports and instantiates a executor class based on naming convention.
