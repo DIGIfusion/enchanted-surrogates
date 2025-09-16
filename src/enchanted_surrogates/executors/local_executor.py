07056619f96230c7918ad92461a9116149f103f5
Amanda <amanda_bruncrona@hotmail.com>
2025-09-16 14:54:00 +0300

    Linting

--- src/enchanted_surrogates/executors/local_executor.py
+++ src/enchanted_surrogates/executors/local_executor.py
@@ -1,18 +1,19 @@
+import os
+import uuid
 from .base_executor import Executor
 from .simulation_task import run_simulation_task
 
-import os 
-import uuid 
 
 class LocalExecutor(Executor):
-    
+
     def start_runs(self):
         while self.sampler.has_budget: 
             samples: list[dict] = self.sampler.get_next_samples()
-            
-            for sample in samples: 
+
+            for sample in samples:
                 sample_run_dir = os.path.join(self.base_run_dir, str(uuid.uuid4()))  # TODO. uuid.uuid should probably have a random seed ? 
-                new_future     = run_simulation_task(self.runner_kwargs, sample_run_dir, params=sample)
+                new_future = run_simulation_task(
+                    self.runner_kwargs, sample_run_dir, params=sample)
                 self.sampler.register_future(new_future)
 
     def clean(self):
