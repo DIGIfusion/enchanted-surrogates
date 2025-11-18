dask executor now accepts scane_n_jobs_min/max and with use this for dasks auto scale feature. I removed all uneeded saved futures incase they were getting in the way of scaling. I fixed some bugs in simulation tasks related to error handeling. I removed text from the plot of sobol g runner. I fexed some issues in gpy_analytic_sobol_sampler, including some numerical issues, tese fixes still need to be tested. Sobol sequence now has repeat and index options.

src/enchanted_surrogates/samplers/gpy_analytic_sobol_sampler.py
@@ -61,8 +61,12 @@ class GpyAnalyticSobolSampler(Sampler):
 
         self.sampling_strategy = kwargs.get('sampling_strategy', 'random')
         self.batch_size = kwargs.get('batch_size', None) or 2
+        self.initial_batch_size = kwargs.get('initial_batch_size', self.batch_size)
+        self.initial_pool_samples_strategy = kwargs.get('initial_pool_samples_strategy', 'random')
         self.seed = kwargs.get('seed', 42)
         self.base_run_dir = kwargs.get('base_run_dir')
+        self.num_repeats = kwargs.get('num_repeats', 1)
+        self.include_index = kwargs.get('include_index', False)
         self.batch_number = 0
         self.submitted = 0
         self.custom_submitted = 0
@@ -121,9 +125,10 @@ class GpyAnalyticSobolSampler(Sampler):
                     if len(collected) >= self.initial_pool_size:
                         break
             if len(collected) == 0:
-                self.pool = rng.uniform(low=[b[0] for b in self.bounds],
-                                        high=[b[1] for b in self.bounds],
-                                        size=(self.initial_pool_size, len(self.bounds)))
+                raise RuntimeError('Pool sampler did not provide any points for initial pool.')
+                # self.pool = rng.uniform(low=[b[0] for b in self.bounds],
+                #                         high=[b[1] for b in self.bounds],
+                #                         size=(self.initial_pool_size, len(self.bounds)))
             else:
                 self.pool = np.array(collected, dtype=float)
         else:
@@ -156,24 +161,40 @@ class GpyAnalyticSobolSampler(Sampler):
     def get_initial_samples(self, *args, **kwargs):
         # If a sub-sampler is configured, prefer it for initial samples
         if self.sub_sampler is not None:
-            samples = self.sub_sampler.get_next_samples()
+            samples = self.sub_sampler.get_next_samples() * self.num_repeats
             if samples:
                 self.batch_number += 1
                 self.submitted += len(samples)
                 self.custom_submitted += len(samples)
                 # remove samples from pool if present
                 self._remove_from_pool(samples)
+                if self.include_index:
+                    samples = [
+                        {**samp, 'index': ind} for samp, ind in zip(samples, range(len(samples)))]
                 return samples
+            else: return None
 
         # Otherwise return random points from pool
         if self.pool is None or len(self.pool) == 0:
             self._init_pool()
-        rng = np.random.RandomState(self.seed)
-        n = min(self.batch_size, len(self.pool))
-        idxs = rng.choice(len(self.pool), size=n, replace=False)
-        chosen = self.pool[idxs]
-        self.pool = np.delete(self.pool, idxs, axis=0)
-        samples = [{key: float(val) for key, val in zip(self.parameters, row)} for row in chosen]
+        
+        if self.initial_pool_samples_strategy == 'random':
+            rng = np.random.RandomState(self.seed)
+            n = min(self.initial_batch_size, len(self.pool))
+            idxs = rng.choice(len(self.pool), size=n, replace=False)
+            chosen = self.pool[idxs]
+            self.pool = np.delete(self.pool, idxs, axis=0)
+            samples = [{key: float(val) for key, val in zip(self.parameters, row)} for row in chosen] * self.num_repeats
+        elif self.initial_pool_samples_strategy == 'first':
+            n = min(self.initial_batch_size, len(self.pool))
+            chosen = self.pool[:n]
+            self.pool = self.pool[n:]
+            samples = [{key: float(val) for key, val in zip(self.parameters, row)} for row in chosen] * self.num_repeats
+
+        if self.include_index:
+            samples = [
+                {**samp, 'index': ind} for samp, ind in zip(samples, range(len(samples)))]
+
         self.batch_number += 1
         self.submitted += len(samples)
         self.custom_submitted += len(samples)
@@ -378,6 +399,11 @@ class GpyAnalyticSobolSampler(Sampler):
             return None
         if samples is not None:
             self.custom_submitted += len(samples)
+        samples = samples * self.num_repeats
+        if self.include_index:
+            samples = [
+                {**samp, 'index': ind} for samp, ind in zip(samples, range(len(samples)))]
+
         return samples
 
     # ---------------------------

src/enchanted_surrogates/samplers/sobol_sequence.py
@@ -5,12 +5,14 @@ from scipy.stats.qmc import Sobol
 import warnings
 
 class SobolSequence(Sampler):
-    def __init__(self, bounds, budget, parameters, **kwargs):
-        self.budget = budget
-        self.power = int(np.log2(self.budget))
-        if self.budget != 2**self.power:
-            warnings.warn(f'SOBOL SEQUENCE BUDGET MUST BE A POWER OF 2 eg, 2,4,16,32... SETTING BUDGET TO {2**self.power}')
-        self.budget = 2**self.power
+    def __init__(self, bounds, num_samples, parameters, **kwargs):
+        self.num_samples = num_samples
+        self.power = int(np.log2(self.num_samples))
+        if self.num_samples != 2**self.power:
+            warnings.warn(f'SOBOL SEQUENCE NUM_SAMPLES MUST BE A POWER OF 2 eg, 2,4,16,32... SETTING NUM_SAMPLES TO {2**self.power}')
+        self.num_samples = 2**self.power
+        self.num_repeats = kwargs.get("num_repeats", 1)
+        self.budget = self.num_samples * self.num_repeats
         
         self.bounds = bounds
         self.parameters = parameters
@@ -29,6 +31,7 @@ class SobolSequence(Sampler):
         # TODO not use uniform?
         # TODO batch samples
         samples = [{key: value for key, value in zip(self.parameters, params)} for params in self.samples[self.batch_number*self.batch_size  :  min((self.batch_number+1)*self.batch_size, self.budget)]]
+        samples = samples * self.num_repeats
         # samples = [{key: value for key, value in zip(self.parameters, params)} for params in self.samples[self.batch_number*self.batch_size  :  (self.batch_number+1)*self.batch_size]]
         
         self.batch_number += 1