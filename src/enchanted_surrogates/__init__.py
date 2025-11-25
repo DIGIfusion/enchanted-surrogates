dad1bf3faad1ac53317ab104874870b07dab7658
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-11-25 11:47:29 +0200

    Sometimes plugins we failing silently, so I added a print so when it silently crashes I know which plugin was being loaded. I also added a try catch, so if it doesn't fail silently then the name of the plugin with the error is printed

--- src/enchanted_surrogates/__init__.py
+++ src/enchanted_surrogates/__init__.py
@@ -1,8 +1,13 @@
-from importlib.metadata import entry_points
-
+from importlib_metadata import entry_points
 
 def load_plugins():
     """Load all plugins registered under the 'enchanted_surrogates' entry points."""
     eps = (list(entry_points(group="enchanted_surrogates.parsers"))
            + list(entry_points(group="enchanted_surrogates.runners")))
+    for ep in eps:
+        print(f"Loading plugin: {ep.name}")
+        try:
+            ep.load()
+        except Exception as e:
+            print(f"Error loading plugin {ep.name}: {e}")
     return {ep.name: ep.load() for ep in eps}
