dca481891f9cfc22e4015ddee9ec9f593eba0675
daniel harley jordan <daniel.harley.jordan@outlook.com>
2025-11-25 11:30:10 +0200

    I added prints so when it fails silently while loading a plugin I know which one it was. If the error does show when caught then the name of the plugin with the error is printed too

--- src/enchanted_surrogates/__init__.py
+++ src/enchanted_surrogates/__init__.py
@@ -1,12 +1,5 @@
 from importlib_metadata import entry_points
 
-# def load_plugins():
-#     """Load all plugins registered under the 'enchanted_surrogates' entry points."""
-#     eps = (list(entry_points(group="enchanted_surrogates.parsers"))
-#            + list(entry_points(group="enchanted_surrogates.runners")))
-#     return {ep.name: ep.load() for ep in eps}
-
-
 def load_plugins():
     """Load all plugins registered under the 'enchanted_surrogates' entry points."""
     eps = (list(entry_points(group="enchanted_surrogates.parsers"))
