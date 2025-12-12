b5ffedc6921a2a8591c4e5e50c7f5a539e85dcc3
Touko Seppä <touko.seppa@aalto.fi>
2025-12-12 15:39:19 +0200

    refactor: modified to fit supervisor

--- src/enchanted_surrogates/executors/local_executor.py
+++ src/enchanted_surrogates/executors/local_executor.py
@@ -4,23 +4,11 @@ from enchanted_surrogates.executors.simulation_task import run_simulation_task
 
 class LocalExecutor(Executor):
 
-<<<<<<< HEAD
-    def start_runs(self):
-        self.sampler = import_sampler(
-            sampler_type=self.sampler_config.pop("type"), sampler_config=self.sampler_config)
-        while self.sampler.has_budget:
-            samples: list[dict] = self.sampler.get_next_samples()
-=======
-    """
-    Docstring for LocalExecutor
-    """
-    def start_runs(self, input: list[(str, dict)]):
->>>>>>> 71fc318 (change: moved functionalities to supervisor)
-
+    def execute(self, input: list[(str, dict)], sampler):
         for run_dir, sample in input:
             new_future = run_simulation_task(
                 self.runner_config, run_dir, params=sample)
-            self.sampler.register_future(new_future)
+            sampler.register_future(new_future)
 
     def clean(self):
         print('Local runner doesn\'t clean up any resources')
