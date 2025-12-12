e5555d124d3f2e8d3214c67f00a5b90614e0f98d
Touko Seppä <touko.seppa@aalto.fi>
2025-12-12 15:39:19 +0200

    refactor: modified to fit supervisor

--- src/enchanted_surrogates/executors/local_executor.py
+++ src/enchanted_surrogates/executors/local_executor.py
@@ -7,12 +7,12 @@ class LocalExecutor(Executor):
     """
     Docstring for LocalExecutor
     """
-    def start_runs(self, input: list[(str, dict)]):
+    def execute(self, input: list[(str, dict)], sampler):
 
         for run_dir, sample in input:
             new_future = run_simulation_task(
                 self.runner_config, run_dir, params=sample)
-            self.sampler.register_future(new_future)
+            sampler.register_future(new_future)
 
     def clean(self):
         print('Local runner doesn\'t clean up any resources')
