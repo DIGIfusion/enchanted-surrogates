f8ffcdea3906c45172d14cdb7f6859d3d68dc96c
Juha Vanhala <juha.vanhala@aalto.fi>
2026-01-20 15:32:54 +0200

    refactor: removed unused code

--- src/run.py
+++ src/run.py
@@ -28,18 +28,6 @@ def load_configuration(config_path: str) -> argparse.Namespace:
     config = argparse.Namespace(**config)
     config.supervisor["config_filepath"] = config_path
 
-    # In case sampler or runner is defined outside the executor.
-    # This only works for non nested workflows.
-    #if 'sampler_config' not in config.executor:
-    #    if getattr(config, 'sampler', None):
-    #        config.executor['sampler_config'] = config.sampler
-    #    elif 'sampler' in config.executor:
-    #        config.executor['sampler_config'] = config.executor.pop('sampler')
-    #if 'runner_config' not in config.executor:
-    #    if getattr(config, 'runner', None):
-    #        config.executor['runner_config'] = config.runner
-    #    elif 'runner' in config.executor:
-    #        config.executor['runner_config'] = config.executor.pop('runner')
     dask_print(config)
     return config
 
