f9b1d18b17d4a060163b238b9ef473ced5289649
Juha Vanhala <juha.vanhala@aalto.fi>
2026-01-20 15:24:18 +0200

    fix: Changed runner_config to be optional constructor argument of executors

--- src/enchanted_surrogates/executors/base_executor.py
+++ src/enchanted_surrogates/executors/base_executor.py
@@ -3,10 +3,9 @@ from abc import ABC, abstractmethod
 
 class Executor(ABC):
     def __init__(
-        self, output_dir=None, *args, **kwargs
+        self, runner_config=None, output_dir=None, *args, **kwargs
     ):
-        #self.sampler_config = sampler_config
-        #self.runner_config = runner_config
+        self.runner_config = runner_config
         self.output_dir = output_dir  # TODO rename
 
     @abstractmethod
@@ -16,6 +15,3 @@ class Executor(ABC):
     @abstractmethod
     def clean(self):
         raise NotImplementedError("clean method not implemented.")
-
-    def set_runner_config(self, config):
-        self.runner_config = config
