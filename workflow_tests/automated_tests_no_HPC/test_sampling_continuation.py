c683c37a11cdde5550285720fe51916fa3e2ce0d
Juha Vanhala <juha.vanhala@aalto.fi>
2026-03-03 13:06:36 +0200

    test: Moved seamless sampling tests into its own file and created test case for nested resume

---
+++ workflow_tests/automated_tests_no_HPC/test_sampling_continuation.py
@@ -0,0 +1,114 @@
+"""
+Full workflow tests for seamless sampling continuation.
+"""
+
+import os
+
+from types import SimpleNamespace
+from enchanted_surrogates.executors import LocalExecutor, JoblibExecutor, DaskExecutor
+from enchanted_surrogates.supervisor.supervisor import Supervisor
+
+executor_config = {"type": "LocalExecutor"}
+runner_config = {"type": "ExampleRunner"}
+
+def test_simple_resume_after_interruption(tmp_path):
+    bounds = [[-5, 5], [0, 1]]
+    parameters = ["c1", "c2"]
+    budget = 50
+    kill_after = 4
+    batch_size = 10
+    sampler_config = {
+        "type": "RandomSamplerWithKill",
+        "bounds": bounds,
+        "budget": budget,
+        "parameters": parameters,
+        "batch_size": batch_size,
+        "kill_after": kill_after,  # kills when sample 4 is done
+    }
+
+    base_run_dir = tmp_path
+
+    args = SimpleNamespace(
+        executors={"e1": executor_config},
+        samplers={"s1": sampler_config},
+        runners={"r1": runner_config},
+        supervisor={
+            "run_mode": "fresh",
+            "base_run_dir": base_run_dir,
+            "run_order": [{"executor": "e1", "sampler": "s1", "runner": "r1"}],
+        },
+    )
+
+    supervisor = Supervisor(args)
+    try:
+        supervisor.start()
+        assert False  # This shouldn't happen
+    except Exception:
+        pass  # This should happen
+
+    assert len(next(os.walk(tmp_path))[1]) == kill_after * batch_size
+
+    args.supervisor["run_mode"] = "resume"  # continue where the previous one left off
+    args.samplers["s1"]["type"] = "RandomSamplerWithKill"
+    args.samplers["s1"]["kill_after"] = None  # don't kill the poor sampler
+
+    supervisor2 = Supervisor(args)
+    supervisor2.start()
+
+    assert len(next(os.walk(tmp_path))[1]) == budget
+
+def test_nested_resume_after_interruption(tmp_path):
+    bounds = [[-5, 5], [0, 1]]
+    budget = 9
+    batch_size = 3
+    kill_after = 2
+    sampler_config_first = {
+        "type": "RandomSampler",
+        "bounds": bounds,
+        "budget": budget,
+        "parameters": ["c1", "c2"],
+        "batch_size": batch_size
+    }
+    sampler_config_second = {
+        "type": "RandomSamplerWithKill",
+        "bounds": bounds,
+        "budget": budget,
+        "parameters": ["c3", "c4"],
+        "batch_size": batch_size,
+        "kill_after": kill_after
+    }
+
+    base_run_dir = tmp_path
+
+    args = SimpleNamespace(
+        executors={"e1": executor_config},
+        samplers={"s1": sampler_config_first, "s2": sampler_config_second},
+        runners={"r1": runner_config},
+        supervisor={
+            "run_mode": "fresh",
+            "base_run_dir": base_run_dir,
+            "run_order": [
+                {"executor": "e1", "sampler": "s1", "runner": "r1"},
+                {"executor": "e1", "sampler": "s2", "runner": "r1"}
+            ],
+        },
+    )
+
+    supervisor = Supervisor(args)
+    try:
+        supervisor.start()
+        assert False  # This shouldn't happen
+    except Exception:
+        pass  # This should happen
+
+    assert len(next(os.walk(tmp_path))[1]) == budget + budget * kill_after * batch_size
+
+    args.supervisor["run_mode"] = "resume"  # continue where the previous one left off
+    args.samplers["s1"]["type"] = "RandomSampler"
+    args.samplers["s2"]["type"] = "RandomSamplerWithKill"
+    args.samplers["s2"]["kill_after"] = None  # don't kill the poor sampler
+
+    supervisor2 = Supervisor(args)
+    supervisor2.start()
+
+    assert len(next(os.walk(tmp_path))[1]) == budget + budget * budget
--- workflow_tests/automated_tests_no_HPC/test_simple_workflow.py
+++ workflow_tests/automated_tests_no_HPC/test_simple_workflow.py
@@ -81,59 +81,6 @@ def test_full_workflow_joblib(tmp_path):
 
     assert len(next(os.walk(tmp_path))[1]) == budget
 
-
-def test_seamless_sampling(tmp_path):
-    config = {}
-    # -- sampler
-    # TODO: test different samplers
-    bounds = [[-5, 5], [0, 1]]
-    parameters = ["c1", "c2"]
-    budget = 50
-    kill_after = 4
-    batch_size = 10
-    executor_config = {"type": "JoblibExecutor"}
-    sampler_config = {
-        "type": "RandomSamplerWithKill",
-        "bounds": bounds,
-        "budget": budget,
-        "parameters": parameters,
-        "batch_size": batch_size,
-        "kill_after": kill_after,  # kills when sample 4 is done
-    }
-    runner_config = {"type": "ExampleRunner"}
-
-    base_run_dir = tmp_path
-
-    args = SimpleNamespace(
-        executors={"e1": executor_config},
-        samplers={"s1": sampler_config},
-        runners={"r1": runner_config},
-        supervisor={
-            "run_mode": "fresh",
-            "base_run_dir": base_run_dir,
-            "run_order": [{"executor": "e1", "sampler": "s1", "runner": "r1"}],
-        },
-    )
-
-    supervisor = Supervisor(args)
-    try:
-        supervisor.start()
-        assert False  # This shouldn't happen
-    except Exception:
-        pass  # This should happen
-
-    assert len(next(os.walk(tmp_path))[1]) == kill_after * batch_size
-
-    args.supervisor["run_mode"] = "resume"  # continue where the previous one left off
-    args.samplers["s1"]["type"] = "RandomSamplerWithKill"
-    args.samplers["s1"]["kill_after"] = None  # don't kill the poor sampler
-
-    supervisor2 = Supervisor(args)
-    supervisor2.start()
-
-    assert len(next(os.walk(tmp_path))[1]) == budget
-
-
 def test_full_workflow_dask(tmp_path):
     config = {}
     # -- sampler
