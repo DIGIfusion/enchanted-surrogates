24d9926faf9ffd21c796b5606d77b5f77a067c01
Juha Vanhala <juha.vanhala@aalto.fi>
2026-03-05 13:38:28 +0200

    test: Parametrized simple workflow tests

--- workflow_tests/automated_tests_no_HPC/test_simple_workflow.py
+++ workflow_tests/automated_tests_no_HPC/test_simple_workflow.py
@@ -5,27 +5,13 @@ Each executor should be tested in a separate function to avoid conflicts."""
 import pytest
 from workflow_tests.utils.test_utils import *
 
-def test_full_workflow_local(tmp_path, run_config):
-    config = "test_configs/full_workflow_local.yaml"
-    supervisor = run_config(config)
-    run_group = supervisor.groups[0]
-
-    # This should create {budget} folders
-    assert get_run_dir_count(tmp_path) == run_group.sampler.budget
-
-
-def test_full_workflow_joblib(tmp_path, run_config):
-    config = "test_configs/full_workflow_joblib.yaml"
-    supervisor = run_config(config)
-    run_group = supervisor.groups[0]
-
-    # This should create {budget} folders
-    assert get_run_dir_count(tmp_path) == run_group.sampler.budget
-
-
-def test_full_workflow_dask(tmp_path, run_config):
-    config = "test_configs/full_workflow_dask.yaml"
-    supervisor = run_config(config)
+@pytest.mark.parametrize("config_file", [
+    "test_configs/full_workflow_local.yaml",
+    "test_configs/full_workflow_joblib.yaml",
+    "test_configs/full_workflow_dask.yaml"
+])
+def test_full_workflow(tmp_path, run_config, config_file):
+    supervisor = run_config(config_file)
     run_group = supervisor.groups[0]
 
     # This should create {budget} folders
