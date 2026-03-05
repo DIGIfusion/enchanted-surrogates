c7bdc2a30e00787765f0ec8488c1e9a437e11dc9
Juha Vanhala <juha.vanhala@aalto.fi>
2026-03-05 13:02:24 +0200

    test: fixture can now run from config files

--- workflow_tests/automated_tests_no_HPC/conftest.py
+++ workflow_tests/automated_tests_no_HPC/conftest.py
@@ -1,16 +1,31 @@
 import pytest
+from pathlib import Path
 from enchanted_surrogates.supervisor.supervisor import Supervisor
+from enchanted_surrogates.utils.logger import LoggerConfig
 from run import load_configuration, setup_logger
 
 @pytest.fixture
-def run_config(config_file: str) -> Supervisor:
+def run_config(tmp_path, request):
     """
-    Helper fixture to run the program with the given config file.
+    Factory fixture to run the program with optional args modifications.
     """
-    args = load_configuration(config_file)
-    supervisor = Supervisor(args, config_path=config_file)
+    def _run(config_file: str, args_override=None):
+        test_dir = Path(request.fspath).parent
+        config_path = (test_dir / config_file).resolve()
 
-    setup_logger(supervisor.base_run_dir, "INFO")
+        args = load_configuration(str(config_path))
+        args.supervisor["base_run_dir"] = tmp_path
 
-    supervisor.start()
-    return supervisor
+        if args_override:
+            for section, values in args_override.items():
+                args.__dict__[section].update(values)
+
+        supervisor = Supervisor(args, config_path=config_path)
+        setup_logger(supervisor.base_run_dir, "INFO")
+        supervisor.start()
+
+        return supervisor
+
+    yield _run
+    # Teardown
+    LoggerConfig.reset()
