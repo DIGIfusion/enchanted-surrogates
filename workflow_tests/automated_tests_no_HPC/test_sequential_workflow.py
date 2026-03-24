c792d8546288bff01ad73b6742e81d88109155cf
Juha Vanhala <juha.vanhala@netikka.fi>
2026-03-24 11:01:16 +0200

    test: Implemented simple workflow test for MRSAL sampling

--- workflow_tests/automated_tests_no_HPC/test_sequential_workflow.py
+++ workflow_tests/automated_tests_no_HPC/test_sequential_workflow.py
@@ -4,27 +4,19 @@ Basic tests for the sequential workflow.
 
 import pytest
 from workflow_tests.utils.test_utils import *
-from enchanted_surrogates.runners.base_runner import Runner
-
-class EvalRunner(Runner):
-    def __init__(self, clause: str, **kwargs):
-        self.clause = clause
-
-    def single_code_run(self, run_dir: str, params: dict = None) -> dict:
-        # TODO unpack params so they can be used in eval
-        result = eval(self.clause)
-        # TODO write result
-
+from enchanted_surrogates.supervisor.supervisor import Supervisor
 
 def test_sequential_workflow(tmp_path, run_config):
-    pass
-    return
-
-    supervisor = run_config("test_configs/sequential_local.yaml")
+    supervisor: Supervisor = run_config("test_configs/sequential_local.yaml")
     run_group = supervisor.nested_groups[0]
+    sequences = len(run_group.runners)
 
-    # This should create {budget} folders
-    assert get_run_dir_count(tmp_path / "data") == run_group.sampler.budget
+    # This should create {budget * sequence_count} folders
+    assert get_run_dir_count(tmp_path / "data") == run_group.sampler.budget * sequences
 
-    # TODO: read summary CSV and compare to hardcoded expected csv
+    # Summary file should only contain output from the final sequence
     summary = read_summary_file(tmp_path)
+    assert len(summary) == run_group.sampler.budget
+
+    for row in summary:
+        assert row["output"] == pytest.approx(12.0)
