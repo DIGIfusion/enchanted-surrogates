508a1b06ec76e6a259ab983d178187ef31e4ac82
Juha Vanhala <juha.vanhala@aalto.fi>
2026-04-03 12:26:09 +0300

    docs: Added docstrings for SumRunner

--- src/enchanted_surrogates/runners/sum_runner.py
+++ src/enchanted_surrogates/runners/sum_runner.py
@@ -6,12 +6,26 @@ log = get_logger(__name__)
 
 class SumRunner(Runner):
     """
-    TODO
+    Example runner for sequential (or nested) workflows. Sums parameters together.
+    
+    Allows different way of operation based on parameters given in config files, which makes
+    this useful for workflow testing. In real use, it would make more sense to implement 
+    separate runners for different purposes.
+
+    Example use:
+    - Samples for three parameters, `c1`, `c2` and `c3` are generated
+    - First SumRunner is defined with `params_to_sum: ['c1', 'c2']`
+        - Result of `single_code_run` will be `c1 + c2`
+    - Second SumRunner is defined with `params_to_sum: ['output', 'c3']`
+        - Result of `single_code_run` will be `result_of_first_runner + c3`
     """
 
     def __init__(self, params_to_sum: list[str], **kwargs):
         """
-        TODO docstring
+        Args:
+            params_to_sum (list[str]): List of parameter names
+            fail_if (int, optional): Runner fails if calculated sum equals this value. 
+                None by default
         """
         self.params_to_sum = params_to_sum
         self.fail_if = kwargs.get("fail_if", None)
@@ -19,6 +33,7 @@ class SumRunner(Runner):
     def single_code_run(self, run_dir: str, params: dict = None) -> dict:
         """
         Sums all params together (if they are in the list given to init function).
+        Also writes all the parameters received as input into a text file.
 
         Args:
             run_dir (str): Run directory of this instance
@@ -43,7 +58,8 @@ class SumRunner(Runner):
             if key in self.params_to_sum:
                 output += value
 
-        if self.fail_if and abs(output - self.fail_if) < 0.25:
+        epsilon = 0.25
+        if self.fail_if and abs(output - self.fail_if) < epsilon:
             success = False
 
         return {
